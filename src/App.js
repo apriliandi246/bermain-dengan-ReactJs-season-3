3d582807ea2bd607776367fbb9b9576fb61e9398
apriliandi <apriliandi642@gmail.com>
2020-07-04 23:15:48 +0700

    add loader spinner

--- src/App.js
+++ src/App.js
@@ -2,46 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 
 const MovieNotFound = () => {
-  return <h1 className="not-found">Movie Not Found</h1>
+  return (
+    <div className="not-found">
+      <h1>Movie Not Found</h1>
+    </div>
+  );
 }
 
-const App = () => {
-  const [text, setText] = useState('');
-  const [data, setData] = useState([]);
-
-  const handleInput = (event) => {
-    setText(event.target.value);
-  }
-
-  const handleSearch = (event) => {
-    event.preventDefault();
-
-    if (text === '') {
-      alert('type something');
-      return;
-    }
-
-    fetch(`http://www.omdbapi.com/?apikey=80dfc363&s=${text}`)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => {
-        return false;
-      });
-  }
-
+const Movies = ({ data }) => {
   return (
-    <div>
-      <div className="form">
-        <form onSubmit={handleSearch}>
-          <input type="text" placeholder="search movie..." onChange={handleInput} />
-          <button>Search</button>
-        </form>
-      </div>
-
-      {data.Response === 'False' && <MovieNotFound />}
-
+    <React.Fragment>
       <div className="list-movie">
-        {data.Search && (
+        {
           data.Search.map((movie) => {
             return (
               <div className="movie" key={movie.imdbID}>
@@ -50,10 +22,64 @@ const App = () => {
                 <p>{movie.Year}</p>
                 <p>{movie.Type}</p>
               </div>
-            )
+            );
           })
-        )}
+        }
       </div>
+    </React.Fragment>
+  );
+}
+
+const App = () => {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+  const [status, setStatus] = useState(false);
+
+  const handleInput = (event) => {
+    setText(event.target.value);
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    if (!text) {
+      alert('type something...');
+      return;
+    }
+
+    setData([]);
+    setStatus(true);
+
+    fetch(`http://www.omdbapi.com/?apikey=80dfc363&s=${text}`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  let movies;
+
+  if (data.Search) {
+    movies = <Movies data={data} />
+  } else {
+    if (data.Response === 'False') {
+      movies = <MovieNotFound />
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSearch}>
+          <div class="input-form">
+            <input type="text" placeholder="search movie..." onChange={handleInput} />
+            <button>Search</button>
+          </div>
+        </form>
+      </div>
+
+      {status && data.length === 0 ? <div className="spinner-2"></div> : movies}
     </div>
   );
 }
